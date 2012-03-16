aeedf0f321e078ab71a3912139275934b218413e
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-16 02:28:03 +0200

    rails new paperclip-demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipDemo::Application.config.secret_token = 'd44587247d55bbf7b53dbfd86d5164b21a3c09a518a6b3a013a6226206ee758f736168b18789d763371e2a535f0525e65fc6d2891f99323d14710d680240c399'
