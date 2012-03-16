647229ab9279d0b014604909844fc7b1385b0b8b
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-16 03:31:22 +0200

    fix

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,8 @@
 class Asset < ActiveRecord::Base
-	belongs_to :form
+	belongs_to :article
 
 	has_attached_file :image,
-		:style => {
+		:styles => {
 			:thumb =>  '150x150#',
 			:medium => '300x300>',
 			:large =>  '600x600>'
