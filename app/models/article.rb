c5e6a587cd05ab9c725d825750c2a30584c4b8e5
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-16 02:39:17 +0200

    model:asset+article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+	validates :name, :presence => true
+	validates :content, :presence => true
+	validates :name, :uniqueness => true
+
+	has_many :assets
+	accepts_nested_attributes_for :assets
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,2 +1,10 @@
 class Asset < ActiveRecord::Base
+	belongs_to :form
+
+	has_attached_file :image,
+		:style => {
+			:thumb =>  '150x150#',
+			:medium => '300x300>',
+			:large =>  '600x600>'
+	}
 end
